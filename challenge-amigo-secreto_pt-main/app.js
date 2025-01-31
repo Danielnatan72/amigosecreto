18cc7833e3f4df05d1187d57c486045190a727a0
Lays Fonseca <laysfonseca86@gmai.com>
2025-01-31 18:55:26 -0300

    Primeiro commit - projeto inicial v1

--- challenge-amigo-secreto_pt-main/app.js
+++ challenge-amigo-secreto_pt-main/app.js
@@ -1,1 +1,58 @@
-//O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+// usuarios inserir nomes no campo de placeholder
+let amigos = [];
+let lista = document.getElementById('listaAmigos');
+lista.innerHTML = '';
+for (let i = 0; i < amigos.length; i++){
+    let item = document.createElement('li')
+}
+
+
+// adicionar nome na lista clicando em "adicionar"
+function adicionarAmigo(){
+    let campoNome = document.getElementById('amigo')
+    let nome = campoNome.value;
+    if (nome === ''){
+        alert('Por favor, insira um nome.')
+        return;
+    }
+        if (amigos.includes(nome)) {
+            alert('Nome duplicado, insira outro');
+            return
+        }
+    amigos.push(nome)
+    limparCampo();
+    AtualizarLista();
+    console.log(nome);
+} 
+
+function AtualizarLista(){
+    let lista = document.getElementById('listaAmigos')
+    lista.innerHTML = '';
+    amigos.forEach(function(nome) {
+    lista.innerHTML += `<li>${nome}</li>`
+    })
+    }
+
+AtualizarLista();
+
+// botão "sortear amigo" com função de sortear um nome recebido da lista de maneira aleatoria
+function sortearAmigo(){
+    if (amigos.length === 0){
+        alert('A lista de amigos esta vazia! Adicione um nome para sortear')
+        return;
+    }
+    let amigosAleatorios = Math.floor(Math.random() * amigos.length);
+    let amigoSorteado = amigos[amigosAleatorios];
+    document.getElementById("resultado").innerHTML = `🎉 O amigo secreto é: <strong>${amigoSorteado}</strong>`;
+}
+
+// realizar sorteio aleatorio e determinar quem é o amigo secreto
+function sortearNomeAleatorio(){
+    return (Math.floor)(amigos)
+}
+
+// limpar o campo de entrada
+function limparCampo(){
+    nome = document.getElementById('amigo')
+    nome.value = '';
+}
